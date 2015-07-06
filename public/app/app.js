37a656c2769189493b95a1dbab9efb110e1aa6f6
Pavel Polyakov <pavel@kredito.de>
2015-07-06 23:19:11 +0200

    vue.js experiments

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,50 @@
-new Vue({
-    el: 'body',
-    data: {
-        salutation: ', hurray!'
-    },
-    filters: {
+// response component, draws the
+Vue.component('response', {
+    template: '#response-template',
+    compiled: function () {
 
+    },
+    data    : function () {
+        console.log('uuid', this.uuid);
+        return {}
+    },
+    methods : {
+        addVerb: function () {
+
+        }
     }
 });
+
+// application builder, one per application
+var builder = new Vue({
+    el      : '.builder',
+    data    : {
+        responses: []
+    },
+    methods : {
+        addResponse: function () {
+            if (this.isMaxResponses) {
+                return false;
+            }
+
+            console.log('adding ' + (this.responses.length + 1));
+            var data = {uuid: UUID.generate()};
+            console.log(data);
+            this.responses.push(data);
+        }
+    },
+    computed: {
+        isMaxResponses: function () {
+            return this.responses.length === 5;
+        }
+    }
+});
+
+new Vue({
+    el      : 'body',
+    data    : {
+        salutation: ', hurray!'
+    },
+    children: [builder]
+});
+
