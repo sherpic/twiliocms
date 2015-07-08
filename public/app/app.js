3cdf838c0b0377be1170038c4cae3d2ed2b3bd9a
Pavel Polyakov <pavel@kredito.de>
2015-07-08 22:44:40 +0200

    getting familiar with vue.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,20 +1,61 @@
 // response component, draws the
 Vue.component('response', {
     template: '#response-template',
+    props: [
+        // you can use prop assertions to ensure the
+        // callback prop is indeed a function.
+        {
+            name: 'remove-response',
+            type: Function,
+            required: true
+        }
+    ],
     compiled: function () {
 
     },
     data    : function () {
-        console.log('uuid', this.uuid);
-        return {}
+        return {
+            verbs: []
+        }
     },
     ready   : function () {
-        // it works
-        console.log(this.uuid);
+
     },
     methods : {
         addVerb: function () {
-
+            console.log('We are going to add the verb, beware!');
+        },
+        remove: function(response) {
+            // triggering the parent method
+            this.removeResponse(this.$data);
+        }
+    },
+    components: {
+        'title': {
+            props: ['title'],
+            template: '<div v-if="!editMode">\n<span class="panel-title">{{title}}</span>\n    <a v-on="click: edit" style="cursor: pointer;">[e]</a>\n</div>\n<div v-if="editMode">\n    <input v-model="tmpTitle">\n    <a v-on="click: save">[s]</a>\n</div>\n',
+            data  : function() {
+                return {
+                    editMode: false,
+                    tmpTitle: ''
+                };
+            },
+            ready: function() {
+                this.tmpTitle=this.title;
+            },
+            methods: {
+                edit: function() {
+                    console.log('edit');
+                    this.editMode = true;
+                },
+                save: function() {
+                    console.log('save');
+                    if(!_.isEmpty(this.tmpTitle)) {
+                        this.title = this.tmpTitle;
+                    }
+                    this.editMode = false;
+                }
+            }
         }
     }
 });
@@ -32,9 +73,12 @@ var builder = new Vue({
             }
 
             console.log('adding ' + (this.responses.length + 1));
-            var data = {uuid: UUID.generate()};
+            var data = {title: 'response #'+(this.responses.length+1), uuid: UUID.generate()};
             console.log(data);
             this.responses.push(data);
+        },
+        removeResponse: function(response) {
+            this.responses.$remove(response);
         }
     },
     computed: {
